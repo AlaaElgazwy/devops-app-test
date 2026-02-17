54c92eaddc8eeec2336b9c886a064533a7dce82e
Alaa Elgazwy <ai6949347@gmail.com>
2026-02-17 12:30:53 +0200

    initial fix port listen

--- app.js
+++ app.js
@@ -9,6 +9,6 @@ app.get('/health', (req, res) => {
     res.json({ status: 'OK' });
 });
 
-app.listen(3000, () => {
+app.listen(3000,'0.0.0.0', () => {
     console.log('Server running on port 3000');
 });
